bcb3c439b10bfd22fd50da8552dca40156affd07
HS12392huangsen <91825730+HS12392huangsen@users.noreply.github.com>
2021-10-14 11:08:03 +0800

    Add files via upload

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,25 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- 因为keep-alive了，所以不会销毁，再次进来的时候就不会执行created方法了，不过会执行actived方法,
+    所以在详情页Detail.vue中的created不会被执行，造成每次访问的详情页都是同一个，所以应该给详情页移除keep-alive -->
+    <keep-alive exclude="Detail">
+      <router-view></router-view>
+    </keep-alive>
+    <main-tab-bar/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MainTabBar from 'components/content/mainTabbar/MainTabBar'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    MainTabBar
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  @import "assets/css/base.css";
 </style>
